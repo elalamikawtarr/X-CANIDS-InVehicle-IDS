c0bef91648d134311798a4503417872d8ccfba81
elalamikawtarr <elalamikawtar684@gmail.com>
2026-05-19 23:08:03 +0100

    run_diagnostic.py

---
+++ run_diagnostic.py
@@ -0,0 +1,18 @@
+# Préparation des données (X_test_scaled)
+X_test = df_brut.select_dtypes(include=[np.number])
+X_test_scaled = scale_signals_xcanids(X_test)
+
+#  Instanciation du modèle (Correction du NameError)
+input_dim = X_test_scaled.shape[1]
+autoencoder = build_x_canids_autoencoder(input_dim)
+
+# Obtenir la reconstruction
+print(" Analyse du trafic en cours...")
+S_pred = autoencoder.predict(X_test_scaled)
+
+# Calcul de l'erreur par signal
+signalwise_error = np.mean(np.power(X_test_scaled - S_pred, 2), axis=0)
+
+# Identifier le signal suspect
+target_signal_idx = np.argmax(signalwise_error)
+print(f"Attaque détectée sur le signal : {X_test.columns[target_signal_idx]}")
