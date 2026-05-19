3d55227dbab9c424627b6a208b83c5e9950d7fee
elalamikawtarr <elalamikawtar684@gmail.com>
2026-05-19 23:04:08 +0100

    deep_autoencoder.py

---
+++ deep_autoencoder.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def build_x_canids_autoencoder(input_dim):
+    # Architecture basée sur le papier (Encoder-Decoder)
+    model = models.Sequential([
+        # ENCODER
+        layers.Input(shape=(input_dim,)),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(32, activation='relu'),
+        layers.Dense(16, activation='relu'), # Latent space (h)
+        
+        # DECODER
+        layers.Dense(32, activation='relu'),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(input_dim, activation='sigmoid') # Reconstruction de S'
+    ])
+    
+    model.compile(optimizer='adam', loss='mse')
+    return model
